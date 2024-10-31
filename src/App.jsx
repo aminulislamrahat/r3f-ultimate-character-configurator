b726e919c3a0f247c5538776bcf083dea53dd771
rahat aminul <aminulislamrahat000555@gmail.com>
2024-10-31 15:11:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Canvas camera={{
+        position: [3, 3, 3],
+
+      }}
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <color attach="background" args={['#333333']} />
+        <OrbitControls />
+        <mesh>
+          <boxGeometry args={[0.5, 0.5, 0.5]} />
+          <meshNormalMaterial />
+        </mesh>
+
+      </Canvas>
+    </>
+  )
+}
+
+export default App
